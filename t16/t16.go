adddab650d305fe7a2a41fc6e4de2fdd4be53991
unknown <rextail@yandex.ru>
2024-08-25 11:15:42 +0700

    minor fixes

--- t16/t16.go
+++ t16/t16.go
@@ -1,15 +1,51 @@
 package t16
 
+import (
+	"cmp"
+	"slices"
+)
+
 //Реализовать быструю сортировку массива (quicksort) встроенными методами языка.
 
 //Можно написать самому, можно использовать стандартную гошную сортировку, внутри которой
 //уже реализован quicksort.
 
-//В частности, если массив уже изначально отсортирован, то приходим к худшему времени работы O(n^2)
+//Само собой, разработчики Go не могут себе позволить обычный quicksort.
+//Их алгоритм основан на pattern-defeating варианте алгоритма, который в нужный
+//момент меняет стратегию сортировки, тем самым предотвращая худший случай O(n^2)
+
+func QuickByGoTeam[C ~[]E, E cmp.Ordered](arr C) {
+	slices.Sort(arr)
+}
+
+//В такой реализации, если массив уже изначально отсортирован, то приходим к худшему времени работы O(n^2)
 //Если же нет, то при удачном разбиении имеем O(nlog(n))
 
-func Quicksort[C ~[]Cmp, Cmp comparable](arr C, l int, r int) C {
+func Quicksort[C ~[]E, E cmp.Ordered](arr C, l int, r int) {
 	if l < r {
-
+		q := partition(arr, l, r)
+		Quicksort(arr, l, q)
+		Quicksort(arr, q+1, r)
 	}
+}
+
+func partition[C ~[]E, E cmp.Ordered](arr C, l int, r int) int {
+	v := arr[(l+r)/2]
+	i := l
+	j := r
+	for i <= j {
+		for arr[i] < v {
+			i++
+		}
+		for arr[j] > v {
+			j--
+		}
+		if i >= j {
+			break
+		}
+		arr[i], arr[j] = arr[j], arr[i]
+		i++
+		j--
+	}
+	return j
 }
--- t16/t16_test.go
+++ t16/t16_test.go
@@ -1,1 +1,54 @@
 package t16
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestQuicksort(t *testing.T) {
+	t.Run("test integers with standard go quicksort", func(t *testing.T) {
+		testcases := []struct {
+			arr      []int
+			expected []int
+		}{
+			{
+				[]int{5, 3, 2, 1, 2, 7, 6, 10, 9, 8, 13, 11, 12, 15, 14, 16},
+				[]int{1, 2, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+			},
+			{
+				[]int{3, 1, 2, 5, 4},
+				[]int{1, 2, 3, 4, 5},
+			},
+		}
+
+		for _, tc := range testcases {
+			QuickByGoTeam(tc.arr)
+			if slices.Compare(tc.arr, tc.expected) != 0 {
+				t.Errorf("expected %v, got %v", tc.expected, tc.arr)
+			}
+		}
+	})
+	t.Run("test integers", func(t *testing.T) {
+		testcases := []struct {
+			arr      []int
+			expected []int
+		}{
+			{
+				[]int{5, 3, 2, 1, 2, 7, 6, 10, 9, 8, 13, 11, 12, 15, 14, 16},
+				[]int{1, 2, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+			},
+			{
+				[]int{3, 1, 2, 5, 4},
+				[]int{1, 2, 3, 4, 5},
+			},
+		}
+
+		for _, tc := range testcases {
+			Quicksort(tc.arr, 0, len(tc.arr)-1)
+			if slices.Compare(tc.arr, tc.expected) != 0 {
+				t.Errorf("expected %v, got %v", tc.expected, tc.arr)
+			}
+		}
+	})
+
+}
